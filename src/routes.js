1331a03fd57f259abd3c49b7ccd2976691fa317f
zxc1520 <afifghulam21@gmail.com>
2023-03-22 05:49:39 +0700

    Initial Commit

---
+++ src/routes.js
@@ -0,0 +1,37 @@
+const { 
+     addNoteHandler, 
+     getAllNotesHandler, 
+     getNoteByIdHandler, 
+     editNoteByHandler,
+     deleteNoteByIdHandler,
+} = require('./handler');
+
+const routes = [
+     {
+          method: 'POST',
+          path: '/notes',
+          handler: addNoteHandler,
+     },
+     {
+          method: 'GET',
+          path: '/notes',
+          handler: getAllNotesHandler,
+     },
+     {
+          method: 'GET',
+          path: '/notes/{id}',
+          handler: getNoteByIdHandler,
+     },
+     {
+          method: 'PUT',
+          path: '/notes/{id}',
+          handler: editNoteByHandler,
+     },
+     {
+          method: 'DELETE',
+          path: '/notes/{id}',
+          handler: deleteNoteByIdHandler,
+     },
+];
+
+module.exports = routes;
